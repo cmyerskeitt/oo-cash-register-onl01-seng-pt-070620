b5b74cea9e01dbff340464626ab884f3bf1a5a43
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-31 17:57:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,7 +29,7 @@ class CashRegister
    end 
   
   def void_last_transaction 
-    self.total = self.last_total
-    self.items = self.last_items
+    @total = @last_total
+    @items = @last_items
   end
 end 
