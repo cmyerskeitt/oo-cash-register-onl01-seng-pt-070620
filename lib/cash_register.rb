dc4a836ecad194b47e809f352dfab421b6ff8907
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-31 17:20:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class CashRegister
   attr_accessor :discount , :total 
   attr_reader :items
@@ -11,6 +13,7 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @total += price*quantity
+    binding.pry
     @items << title
   end
     
