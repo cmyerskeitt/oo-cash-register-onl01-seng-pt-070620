372a10d23068ec5b62584438a1bbbc35c3501359
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-31 17:17:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
-
+class CashRegister
+  attr_accessor :discount , :total 
+  attr_reader :items
+  
+  
+  def initialize (discount=0) 
+    @discount= discount 
+    @total=0
+    @items=[]
+  end 
+  
+  def add_item(title, price, quantity=1)
+    @total += price*quantity
+  end
+    
+    # 
+    # #quantity.times.do
+    # #@items << title
+  
+  def apply_discount
+    if @discount == 0 
+      "There is no discount to apply."
+    else 
+      @total = @total - (@total / 100 * @discount)
+      "After the discount, the total comes to $#{@total}."
+    end 
+   end 
+  
+  def item 
+    
+end 
